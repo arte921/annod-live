a645811b5d566e731a3f78234b1f9fb803d22f08
Ard-Jan van Etten <ardjanve@gmail.com>
2021-02-03 20:55:15 +0100

    all legs

--- index.js
+++ index.js
@@ -21,13 +21,13 @@ let meesteAfstand = 0;
 
 const berekenRitjes = async (aankomstTijd, station, negeerbareFeaturesReferentie, huidigeAfstand, routeTotNuToe) => {
     const vroegsteVertrektijd = new Date(aankomstTijd.getTime() + config.minimum_overstaptijd_seconden * 1000);
-
     // check of rit tot nu toe nog voldoet
     if (vroegsteVertrektijd > eindDatum ||
         (routeTotNuToe.length >= config.snelheidsmetingen_begin_ritjes &&
-            huidigeAfstand / (vroegsteVertrektijd - startDatum) / 3600000 < config.minimum_gemiddelde_snelheid)) return;
+            huidigeAfstand / ((vroegsteVertrektijd - startDatum) / 3600000) < config.minimum_gemiddelde_snelheid)) return;
     
 
+    const laatsteVertrekTijd = new Date(aankomstTijd.getTime() + config.maximum_overstaptijd_seconden * 1000);
     let negeerbareFeatures = [...negeerbareFeaturesReferentie];
     
     kandidaatRoutes.push({
@@ -38,7 +38,7 @@ const berekenRitjes = async (aankomstTijd, station, negeerbareFeaturesReferentie
 
     if (huidigeAfstand > meesteAfstand) {
         meesteAfstand = huidigeAfstand;
-        console.log(huidigeAfstand, routeTotNuToe);
+        console.log(huidigeAfstand, aankomstTijd, routeTotNuToe);
     }
 
     let ritjes = await haalStationOp(station);
@@ -46,7 +46,7 @@ const berekenRitjes = async (aankomstTijd, station, negeerbareFeaturesReferentie
     let berekendeVertrekken = [];
 
     const gefilterdeRitjes = ritjes.departures.filter((rit) => config.toegestane_treintypen.includes(rit.trainCategory));
-
+    // console.log("E");
     for (const rit of gefilterdeRitjes) {
         if (berekendeVertrekken.includes(rit.direction)) continue;
         berekendeVertrekken.push(rit.direction);
@@ -57,37 +57,41 @@ const berekenRitjes = async (aankomstTijd, station, negeerbareFeaturesReferentie
             // console.log("===========MORE THAN ONE LEG=============", volledigeritRaw.trips[0].legs.length);
             continue;
         }
-        const volledigeRitLeg = volledigeritRaw.trips[0].legs[0];
 
         let vorigeStationCode = "";
         let afstand = huidigeAfstand;
 
-        for (const [index, station] of volledigeRitLeg.stops.entries()) {
-            const huidigStation = vindStation(station.name);
-            if (!huidigStation) continue;
+        for (const leg of volledigeritRaw.trips[0].legs) {
+            if (leg.origin.plannedDateTime > laatsteVertrekTijd) continue;
+            for (const [index, station] of leg.stops.entries()) {
+                const huidigStation = vindStation(station.name);
+                if (!huidigStation) continue;
 
-            if (index == 0) {
+                if (index == 0) {
+                    vorigeStationCode = huidigStation.code;
+                    continue;
+                }
+                
+                if (huidigStation.land == "D") return;
+                
+                afstand += stationAfstand(vorigeStationCode, huidigStation.code, negeerbareFeatures);
+
+                // er wordt op het station gestopt
+                if (!station.passing) {
+                    berekenRitjes(new Date(station.plannedArrivalDateTime), huidigStation.code, negeerbareFeatures, afstand, [...routeTotNuToe, station.name]);
+                }
+
                 vorigeStationCode = huidigStation.code;
-                continue;
             }
-            
-            if (huidigStation.land == "D") return;
-            
-            afstand += stationAfstand(vorigeStationCode, huidigStation.code, negeerbareFeatures);
-
-            // er wordt op het station gestopt
-            if (!station.passing) {
-                await berekenRitjes(new Date(station.plannedArrivalDateTime), huidigStation.code, negeerbareFeatures, afstand, [...routeTotNuToe, station.name]);
-            }
-
-            vorigeStationCode = huidigStation.code;
         }
     }
 };
 
-(async () => {
-    await berekenRitjes(startDatum, config.start_station, [], 0, []);
-    console.log(`${kandidaatRoutes.length} routes gevonden in ${(new Date() - programmaStartDatum) / 1000} seconden`);
-    kandidaatRoutes.sort((a, b) => b.afstand - a.afstand);
-    writeJSONSync(kandidaatRoutes, 'resultaat');
-})();
+// (async () => {
+//     await berekenRitjes(startDatum, config.start_station, [], 0, []);
+//     // console.log(`${kandidaatRoutes.length} routes gevonden in ${(new Date() - programmaStartDatum) / 1000} seconden`);
+//     // kandidaatRoutes.sort((a, b) => b.afstand - a.afstand);
+//     // writeJSONSync(kandidaatRoutes, 'resultaat');
+// })();
+
+berekenRitjes(startDatum, config.start_station, [], 0, []);
