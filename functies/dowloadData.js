bb77429f73ece9f2ae443989bfc06e88fd8b4167
Ard-Jan van Etten <ardjanve@gmail.com>
2021-02-04 10:32:37 +0100

    idk

--- functies/dowloadData.js
+++ functies/dowloadData.js
@@ -1,13 +1,26 @@
 const fs = require("fs");
 const path = require("path");
 const haalDataOp = require('./haalDataOp.js');
+const wacht = require('./wacht.js');
 
 module.exports = async (pad, locatie) => {
     let data;
-    do {
+
+    while (true) {
         data = await haalDataOp(pad);
-    } while ((data.statusCode == 429));
+
+        if (data.statusCode == 429) {
+            console.log("ratelimit");
+            await wacht(100);
+            continue;
+        } else if (data.statusCode == 38) {
+            continue;
+        } else {
+            break;
+        }
+    }
 
     if (locatie != null) await fs.promises.writeFile(path.join("opslag", locatie + ".json"), JSON.stringify(data));
+
     return data;
 };
--- functies/haalDataOp.js
+++ functies/haalDataOp.js
@@ -3,8 +3,8 @@ const https = require('https');
 const readJSONSync = require('./readJSONSync.js');
 const config = readJSONSync("config");
 
-module.exports = (pad, json = true) => {
-
+module.exports = (pad) => {
+    console.log("e");
     const options = {
         host: 'gateway.apiportal.ns.nl',
         path: pad,
@@ -17,9 +17,9 @@ module.exports = (pad, json = true) => {
     return new Promise ((resolve, reject) => {
             const request = https.request(options, (response) => {
                 response.on('data', (deel) => antwoord += deel);
-                response.on('end', () => resolve(json ? JSON.parse(antwoord) : antwoord));
+                response.on('end', () => JSON.parse(antwoord));
             });
-            request.on('error', () => resolve({statusCode: 429}));
+            request.on('error', () => resolve({statusCode: 38}));
             request.end();
         }
     );
