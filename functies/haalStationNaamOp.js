e07cb80e7ecef240ac39bf4d3188943ebccb67c4
Ard-Jan van Etten <ardjanve@gmail.com>
2021-02-10 08:00:03 +0100

    EE

--- functies/haalStationNaamOp.js
+++
@@ -1,8 +0,0 @@
-const leesCSVSync = require('./leesCSVSync.js');
-
-const stations = leesCSVSync("stations");
-// console.log(stations);
-
-stations.forEach(console.log);
-
-module.exports = (code) => stations[0].find((kandidaat) => kandidaat[0].replace(" ", "") == code)[8];
--- localplanner.js
+++ localplanner.js
@@ -1,7 +1,10 @@
 const leesIFFSync = require('./functies/leesIFFSync.js');
-const haalStationNaamOp = require('./functies/haalStationNaamOp.js');
+const readJSONSync = require('./functies/readJSONSync.js');
+
+const config = readJSONSync("config");
 
 const splitRegels = (tekst) => tekst.split(/\r?\n/);
+const stripSpaties = (tekst) => tekst.replace(/ +$/, "");
 
 const eindStation = (rit) => {
     const regels = splitRegels(rit);
@@ -13,35 +16,65 @@ const eindStation = (rit) => {
     }
 }
 
-const stopStations = (rit) => {
-    let resultaat = [];
+// rekent eindstation standaard niet mee
+// > beginstation
+// . korte stop
+// + lange stop
+// < eindstation
+const stopStations = (rit, stationstypen = [">", ".", "+"]) => splitRegels(rit)
+    .filter((regel) => stationstypen.includes(regel.charAt(0)))
+    .map((regel) => /[^ ]*/.exec(regel.substring(1)).toString());
+
+
+const vertrekTijd = (rit, station) => {
     const regels = splitRegels(rit);
     for (const regel of regels) {
-        // begin, korte stop, lange stop, eind
-        if ([">", ".", "+", "<"].includes(regel.charAt(0))) {
-            const regex = /[^ ]*/;
-            resultaat.push(regex.exec(regel.substring(1)).toString());
+        const waarden = regel.slice(1).split(',');
+        if (stripSpaties(waarden[0]) == station) {
+            if ([">", "."].includes(regel.charAt(0))) {
+                return waarden[1];
+            } else if (regel.charAt(0) == "+") {
+                return waarden[1];
+            }
         }
     }
-    return resultaat;
 }
 
+const tijdNaarMinutenGetal = (tijd) => (60 * tijd.substring(0, 2) - 0) + (tijd.substring(2, 4) - 0);
+
 const dienstregeling = leesIFFSync('timetbls').split("#").map((entry) => "#" + entry).slice(1);
+
 const stations = splitRegels(leesIFFSync('stations'))
     .slice(1)
-    .map((regel) => regel.split(',').map((waarde) => waarde.replace(/ +$/, "")))
-    // .filter((kandidaat) => kandidaat[4] == "NL");
+    .map((regel) => regel.split(',').map(stripSpaties))
+    .filter((kandidaat) => kandidaat[4] == "NL");
 
-// console.log(stations.length);
-
-// console.log(dienstregeling[29676]);
+const haalStationNaamOp = (stationsCode) => stations.find((kandidaat) => stationsCode == kandidaat[1])[9];
 
 const vertrekken = {};
 
-for (const station of stations) {
+for (const rit of dienstregeling) {
+    const stops = stopStations(rit);
+    for (const station of stations) {
+        const stationsCode = station[1];
 
+        if (stops.includes(stationsCode)) {
+            if (!vertrekken[stationsCode]) vertrekken[stationsCode] = [];
+            vertrekken[stationsCode].push({
+                rit: rit,
+                vertrektijd: vertrekTijd(rit, stationsCode)
+            });
+        }
+    }
 }
 
+const haalVertrekkenOp = (station, tijd) => {
+    for (const rit of vertrekken[station]) {
 
+    }
+}
 
-// console.log(eindStation(dienstregeling[29676]));
+// console.log(vertrekken['ah'][100]);
+// console.log(vertrekTijd(dienstregeling[29676], 'op'));
+
+// console.log(dienstregeling[29676]);
